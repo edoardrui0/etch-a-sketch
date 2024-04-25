b4f39e4a73d4b0a1463e18cbe6e05b90aefa4c5c
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2024-04-25 14:33:43 -0400

    Add 16x16 grid and hover effect
    Locked to 16x16 grid for the time being
    Added a black hover effect to the individual squares

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const boxContainer = document.querySelector("#boxContainer");
 
 let boxLoop = (num) => {
-  let total = num * num;
-  for (let i = 0; i < total; i++) {
+  let total = num * num + num + 1;
+  for (let i = 1; i < total; i++) {
     const boxes = document.createElement("div");
     boxes.classList.add("boxStyle");
     boxContainer.appendChild(boxes);
+
+    boxes.addEventListener("mouseover", (e) => {
+      e.target.style.background = "black";
+    });
   }
 };
 
