d77f2d2420d7f3aed40c6b05bae78f3aa42261ec
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2024-05-09 15:25:59 -0400

    createGrid func has bene added
    createGrid does exactly what the name implies. Input a number, and it'll create the grid

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let createBoxes = (value) => {
   boxes.style.height = size + "px";
   boxes.style.width = size + "px";
   boxes.style.backgroundColor = "white";
+  boxes.style.border = "0.5px solid black";
   // boxes.style.opacity = "0";
 
   for (let i = 0; i < value * value; i++) {
@@ -25,35 +26,15 @@ let createBoxes = (value) => {
   }
 };
 
+let createGrid = () => {
+  btn.addEventListener("click", () => {
+    boxContainer.textContent = "";
+    let value = parseInt(input.value);
+    input.value = "";
+    createBoxes(value);
+    console.log(boxContainer);
+  });
+};
+
 createBoxes(16);
-
-// function boxLoop(num) {
-//   const total = num * num + num;
-//   const mod = num + 1;
-
-//   for (let i = 1; i < total; i++) {
-//     const boxes = document.createElement("div");
-//     boxes.classList.add("boxStyle");
-
-//     if (i % mod === 0) {
-//       boxes.style.cssText = "border: 0; height: 0; width: 100%";
-//     } else {
-//       boxes.style.cssText =
-//         "border: 1px solid black; height: 25px; width: 25px; flex: 1";
-//     }
-
-//     boxContainer.appendChild(boxes);
-
-// boxes.addEventListener("mouseover", (event) => {
-//   event.target.style.background = "black";
-// });
-
-//     btn.addEventListener("click", () => {
-//       boxes.remove();
-//       boxLoop(parseInt(input.value));
-//       input.value = "";
-//     });
-//   }
-// }
-
-// boxLoop(16);
+createGrid();
